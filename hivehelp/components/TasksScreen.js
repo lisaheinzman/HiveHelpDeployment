3f0839c74fed71f2c0181dfe353ef09f61483834
saraschwartz118 <sa181888@ucf.edu>
2024-01-22 15:33:33 -0500

    update

--- hivehelp/components/TasksScreen.js
+++ hivehelp/components/TasksScreen.js
@@ -1,14 +1,32 @@
-// screens/GuidesScreen.js
-
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
 
 const TasksScreen = () => {
+  const [tasks, setTasks] = useState([]);
+
+  // Function to add a task
+  const addTask = (task) => {
+    setTasks([...tasks, task]);
+  };
+
   return (
-    <View>
-      <Text>Tasks Screen</Text>
+    <View style={styles.container}>
+      <Text>This should be the tasks page.</Text>
+      <FlatList
+        data={tasks}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={({ item }) => <Text>{item}</Text>}
+      />
     </View>
   );
-};
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
 export default TasksScreen;
