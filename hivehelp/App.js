d555f460071be26d5807756cd181a3b262f18b6a
Jacob Rothstein <brothstein62@hotmail.com>
2024-01-22 13:13:06 -0500

    show home page

--- hivehelp/App.js
+++ hivehelp/App.js
@@ -1,13 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
+
 import { StyleSheet, Text, View } from 'react-native';
+import HomeScreen from './components/HomeScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>This is where the screens will be imported to and loaded in a HomeStack to navigate between the pages.</Text>
-      <StatusBar style="auto" />
+      <HomeScreen></HomeScreen>
+      <Text>above should be the home page</Text>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
