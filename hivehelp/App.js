89873a1fb6cc2eec9f94a588ae98daa340047163
Jacob Rothstein <brothstein62@hotmail.com>
2024-01-22 13:51:21 -0500

    commit

--- hivehelp/App.js
+++ hivehelp/App.js
@@ -1,12 +1,20 @@
 
 import { StyleSheet, Text, View } from 'react-native';
 import HomeScreen from './components/HomeScreen';
+import CalendarScreen from './components/CalendarScreen';
+import ProfileScreen from './components/ProfileScreen';
+import TasksScreen from './components/TasksScreen';
+import ToDoScreen from './components/ToDoScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>This is where the screens will be imported to and loaded in a HomeStack to navigate between the pages.</Text>
       <HomeScreen></HomeScreen>
+      <CalendarScreen></CalendarScreen>
+      <ProfileScreen></ProfileScreen>
+      <TasksScreen></TasksScreen>
+      <ToDoScreen></ToDoScreen>
     </View>
   )
 }
