5f7d0beec4b947e8e1e2b35f4b568d03c20f76d2
saraschwartz118 <sa181888@ucf.edu>
2024-01-30 19:43:53 -0500

    color update

--- hivehelp/components/GuidesScreen.js
+++ hivehelp/components/GuidesScreen.js
@@ -1,5 +1,3 @@
-// screens/GuidesScreen.js
-
 import React from 'react';
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import { Theme } from './Theme.js'; // Importing the Theme from themes.js
@@ -56,7 +54,7 @@ const styles = StyleSheet.create ({
   },
   title: {
     fontSize: 30,
-    fontWeight: 'bold',
+    //fontWeight: 'bold',
     marginBottom: 10,
     textAlign: 'center'
   },
@@ -67,6 +65,7 @@ const styles = StyleSheet.create ({
     padding: 20,
     marginBottom: 20,
     borderRadius: 10,
+    textAlign: 'center'
   },
   guideTitle: {
     fontSize: 20,
