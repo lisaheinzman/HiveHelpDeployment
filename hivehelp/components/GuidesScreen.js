611fb6fe56f7a1c8265a4da7712d2cccc2b6be24
saraschwartz118 <sa181888@ucf.edu>
2024-01-30 19:32:10 -0500

    color

--- hivehelp/components/GuidesScreen.js
+++ hivehelp/components/GuidesScreen.js
@@ -72,7 +72,7 @@ const styles = StyleSheet.create ({
     fontSize: 20,
     fontWeight: 'bold',
     marginBottom: 8,
-    textAlign: 'center'
+    textAlign: 'center',
   },
   buttons: {
     padding: 10,
